9ba6c5957eb6f9d02b85aec5fc9ba5d56b4b2c8d
Ace-zzzz <escobia357@gmail.com>
2026-01-11 20:26:36 +0800

    feat: JWT setup

--- src/main/java/com/casey/aimihired/config/SecurityConfig.java
+++ src/main/java/com/casey/aimihired/config/SecurityConfig.java
@@ -2,28 +2,47 @@ package com.casey.aimihired.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.casey.aimihired.security.AuthTokenFilter;
 
 @Configuration
 public class SecurityConfig {
+    private final AuthTokenFilter jwtFilter;
+
+    // DEPENDENCY INJECTION
+    public SecurityConfig(AuthTokenFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    // ENCRYPS PASSWORD
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration authConfig) {
+        return authConfig.getAuthenticationManager();
+    }
+
     // CHECK WHICH URLS NEED AUTHORIZATION AND WHICH DON'T
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/users/register").permitAll()
-                .anyRequest().authenticated()
-            );
+                .anyRequest().authenticated())
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    // HASH PASSWORD
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
     }
 }
